aa8092d6a0c667a5fd60b9abb6e7376a8f1b9f16
Sparsh <verencarsparsh@gmail.com>
2025-01-24 16:35:39 +0530

    testing git

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMRI
+          Edit <code>src/App.jsx</code> and save Racnerev
         </p>
       </div>
       <p className="read-the-docs">
